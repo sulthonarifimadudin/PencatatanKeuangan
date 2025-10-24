9f3652647c35acfa703dff9be46196f17b289b6a
sulthonarifimadudin <sulthonarifimadudin@gmail.com>
2025-10-24 01:09:40 +0000

    Add out folder for GitHub Pages static export

---
+++ out/_next/static/chunks/6c4861972988302c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/a8be5b0c6df6feb6.js",
+  "static/chunks/turbopack-efec2364872626e6.js"
+])
---
+++ out/_next/static/chunks/fc5167b7a5f58a2b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a8be5b0c6df6feb6.js",
+  "static/chunks/turbopack-5c727d072258241d.js"
+])
