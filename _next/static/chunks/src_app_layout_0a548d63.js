ba2650f431a66e94acada2fb1cfad964c1f882c2
sulthonarifimadudin <sulthonarifimadudin@gmail.com>
2025-10-24 01:45:57 +0000

    deploy build

--- _next/static/chunks/src_app_layout_0a548d63.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__2dc75fed._.css"
-],
-    source: "dynamic"
-});
